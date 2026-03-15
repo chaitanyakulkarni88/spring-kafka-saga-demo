7a62600291fe994d0713ddc3ce8752feb0b0853c
Chaitanya <chaitanyakulkarni2216@gmail.com>
2026-03-15 11:32:35 +0000

    feat(order-service) - created thread pool task executor

--- order-service/src/main/java/com/example/order/OrderApp.java
+++ order-service/src/main/java/com/example/order/OrderApp.java
@@ -9,6 +9,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.annotation.EnableKafkaStreams;
 import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
 
 @SpringBootApplication
 @EnableKafkaStreams
@@ -43,5 +46,15 @@ public class OrderApp {
                 .partitions(3)
                 .compact()
                 .build();
+    }
+
+    @Bean
+    public Executor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5);
+        executor.setMaxPoolSize(5);
+        executor.setThreadNamePrefix("kafkaSender-");
+        executor.initialize();
+        return executor;
     }
 }
