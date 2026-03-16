a09226a78be5827257a62404ced70ec2b96442ca
Chaitanya <chaitanyakulkarni2216@gmail.com>
2026-03-16 00:02:01 +0000

    feat(order-service) - prometheus scraping config

--- order-service/src/main/java/com/example/order/service/OrderGeneratorService.java
+++ order-service/src/main/java/com/example/order/service/OrderGeneratorService.java
@@ -24,7 +24,7 @@ public class OrderGeneratorService {
 
     @Async
     public void generate() {
-        for (int i = 0; i < 10000; i++) {
+        for (int i = 0; i < 50000; i++) {
             int x = RAND.nextInt(5) + 1;
             Order o = new Order(id.incrementAndGet(), RAND.nextLong(100) + 1, RAND.nextLong(100) + 1, "NEW");
             o.setPrice(100 * x);
